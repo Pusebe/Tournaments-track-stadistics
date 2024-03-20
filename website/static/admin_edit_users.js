5ba751aaa3d96918e475d86320dfb286a4a4c944
Pusebe <serbeise@hotmail.com>
2024-03-20 12:26:13 +0000

    modified:   website/static/admin_edit_users.js 	modified:   website/templates/edit_users.html

--- website/static/admin_edit_users.js
+++ website/static/admin_edit_users.js
@@ -1,7 +1,10 @@
   // Evento de cambio de checkbox
+  var form = document.getElementById('editUsersForm');
+
   $('input[type="checkbox"][name="players"]').change(function() {
     // Desmarca todos los demás checkboxes
     $('input[type="checkbox"][name="players"]').not(this).prop('checked', false);
+    form.action = null
   });
 
 function delete_info(form_path){
@@ -11,7 +14,7 @@ function delete_info(form_path){
   // Actualiza la imagen de perfil si está disponible
   $('#profileImage').attr('src', '');
   //Si se va a crear un nuevo usuario el form debe enviar a otra ruta:
-  var form = document.getElementById('editUsersForm');
+
   form.action = form_path;
 }  
 

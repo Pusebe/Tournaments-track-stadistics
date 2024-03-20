b101afde6936d67ce240d794089ce7d648c04eb3
Pusebe <serbeise@hotmail.com>
2024-03-20 11:23:39 +0000

    modified:   website/static/admin_edit_users.js 	modified:   website/templates/edit_users.html

--- website/static/admin_edit_users.js
+++ website/static/admin_edit_users.js
@@ -9,9 +9,7 @@ function delete_info(){
   $('#email').val("");
   $('#firstName').val("");
   // Actualiza la imagen de perfil si está disponible
-  if (response.photo) {
-    $('#profileImage').attr('src', '');
-  }
+  $('#profileImage').attr('src', '');
   //Si se va a crear un nuevo usuario el form debe enviar a otra ruta:
   var form = document.getElementById('editUsersForm');
   form.action = "{{ url_for('views.edit_users') }}";
